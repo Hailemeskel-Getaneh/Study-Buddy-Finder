632ea7282ac4e374b6415fc91f3ea79e3466535e
Hailemeskel Getaneh <hailegetaneh1221@gmail.com>
2024-12-31 00:41:18 +0300

    user route added

---
+++ Backend/routes/userRoutes.js
@@ -0,0 +1,63 @@
+import express from 'express';
+import asyncHandler from 'express-async-handler';
+import User from '../models/userModel.js';
+import jwt from 'jsonwebtoken';
+
+const router = express.Router();
+
+// Generate JWT
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '30d' });
+};
+
+// Register User
+router.post(
+  '/register',
+  asyncHandler(async (req, res) => {
+    const { name, email, password } = req.body;
+
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      res.status(400);
+      throw new Error('User already exists');
+    }
+
+    const user = await User.create({ name, email, password });
+
+    if (user) {
+      res.status(201).json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        token: generateToken(user._id),
+      });
+    } else {
+      res.status(400);
+      throw new Error('Invalid user data');
+    }
+  })
+);
+
+// Login User
+router.post(
+  '/login',
+  asyncHandler(async (req, res) => {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (user && (await user.matchPassword(password))) {
+      res.json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        token: generateToken(user._id),
+      });
+    } else {
+      res.status(401);
+      throw new Error('Invalidd email or password');
+    }
+  })
+);
+
+export default router;
