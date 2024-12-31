0a7cea0c9feb5491b865113ab21e4a83885a5481
Hailemeskel Getaneh <hailegetaneh1221@gmail.com>
2025-01-01 01:29:08 +0300

    header and footers updated

--- Frontend/src/components/footer.jsx
+++ Frontend/src/components/footer.jsx
@@ -1,11 +1,9 @@
-// src/components/Footer.jsx
 const Footer = () => {
-    return (
-      <footer className="bg-gray-800 text-white text-center p-4">
-        <p>© 2024 Study Buddy Finder. All rights reserved.</p>
-      </footer>
-    );
-  };
-  
-  export default Footer;
-  
+  return (
+    <footer className="bg-gray-800 text-white text-center p-4 mt-8">
+      <p>© 2024 Study Buddy Finder. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
--- Frontend/src/components/header.jsx
+++ Frontend/src/components/header.jsx
@@ -3,12 +3,12 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
-    <header className="bg-blue-600 text-white p-4 flex justify-between items-center">
+    <header className="bg-blue-600 text-white p-4 flex justify-between items-center sticky">
       <h1 className="text-2xl font-bold">
-        <Link to="/">Study Buddy Finder</Link>
+        <Link to="/">SBF</Link>
       </h1>
       <nav>
-      <Link className=" mr-4 hover:underline" to="/">Home</Link>
+      <Link className=" mr-4 bold hover:underline" to="/">Home</Link>
         <Link className="mr-4 hover:underline" to="/login">Login</Link>
         <Link className="hover:underline" to="/register">Register</Link>
       </nav>
