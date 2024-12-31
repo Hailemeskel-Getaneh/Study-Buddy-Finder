2ee0ecfd7c0b86c064e6be90a40f087ae57beaf4
Hailemeskel Getaneh <hailegetaneh1221@gmail.com>
2025-01-01 01:25:19 +0300

    app.jsx is updated with new routes added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,14 +1,26 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Register from './pages/register.jsx';
 import Login from './pages/login.jsx';
-
+import Home from './pages/home.jsx'
+import Profile from './pages/profile.jsx';
+import Mathes from './pages/matches.jsx'
+import './index.css'
 function App() {
   return (
-      <Routes>
-        <Route path="/" element={<h2>Welcome to Study Buddy Finder</h2>} />
+
+    <>
+ 
+
+    <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/mathes" element={<Mathes />} />
+
       </Routes>
+    </>
+
   );
 }
 
