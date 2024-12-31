d615cb26dae90bb49e01313408b251eed599c286
Hailemeskel Getaneh <hailegetaneh1221@gmail.com>
2024-12-31 20:14:43 +0300

    footer component created

---
+++ Frontend/src/components/footer.jsx
@@ -0,0 +1,11 @@
+// src/components/Footer.jsx
+const Footer = () => {
+    return (
+      <footer className="bg-gray-800 text-white text-center p-4">
+        <p>© 2024 Study Buddy Finder. All rights reserved.</p>
+      </footer>
+    );
+  };
+  
+  export default Footer;
+  
