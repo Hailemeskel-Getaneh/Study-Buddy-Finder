481e8855b26fef46cbc392ed1a83d1765d1a8fab
Hailemeskel Getaneh <hailegetaneh1221@gmail.com>
2025-01-02 02:00:48 +0300

    header logo updated

--- Frontend/src/components/header.jsx
+++ Frontend/src/components/header.jsx
@@ -1,16 +1,20 @@
 // src/components/Header.jsx
 import { Link } from 'react-router-dom';
+import Logo from '../assets/images/logo.png';
+
 
 const Header = () => {
   return (
     <header className="bg-blue-600 text-white p-4 flex justify-between items-center sticky">
-      <h1 className="text-2xl font-bold">
-        <Link to="/">SBF</Link>
-      </h1>
+<h4 className="text-xl sm:text-2xl font-extrabold text-white">
+  <Link to="/" className="hover:text-gray-300 transition-colors duration-200">HamarSBF</Link>
+</h4>
+
+
       <nav>
-      <Link className=" mr-4 bold hover:underline" to="/">Home</Link>
-        <Link className="mr-4 hover:underline" to="/login">Login</Link>
-        <Link className="hover:underline" to="/register">Register</Link>
+      <Link className=" mr-4 sm:text-xl bold hover:underline" to="/">Home</Link>
+        <Link className="mr-4 sm:text-xl hover:underline" to="/login">Login</Link>
+        <Link className=" sm:text-xl hover:underline" to="/register">Register</Link>
       </nav>
     </header>
   );
